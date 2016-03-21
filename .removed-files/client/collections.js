aa7002d4311bd0c3d817697426d6297efced0069
zodern <zodern@users.noreply.github.com>
2016-03-20 22:06:25 -0500

    Remove more files

--- .removed-files/client/collections.js
+++
@@ -1,8 +0,0 @@
-PureAdmin.collections = {};
-
-PureAdmin.collections.add = function (name) {
-  if(name in PureAdmin.collections) {
-    return;
-  }
-  PureAdmin.collections[name] = new Mongo.Collection(name);
-};
